e41c2d9a442a24d8ba25fcd5c6d3a63ded2efe39
Grace Waceke <gracewacekemwangi@gmail.com>
2022-03-29 16:48:27 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,54 @@
+//1: Write a function that Adds Two Numbers
+fun main(){
+    var x= addition(40, 40 )
+    println(x)
+
+    var y= square(20, 13)
+    println(y)
+
+    var z= arrayOf("mary", "Carol", "Agnes", "Brian")
+    println(z)
+}
+fun addition(a:Int, b:Int){
+        var sum= a+b
+        println (sum)
+    }
+
+//2:Write a function that finds the square of a number
+fun square(c:Int, d:Int){
+    var comb=c*d
+    println(comb)
+}
+//3:Write a function that displays names
+fun names(cutes: Array<String>){
+    var cute=0
+    var useArray= arrayOf("mary", "Carol", "Agnes", "Brian")
+    cutes.forEach { el->{
+        println(cutes)
+    }
+    }
+}
+//4:Given below is an example of Character  Convert char to String
+fun main(args: Array<String>) {
+   var char=0
+    var 
+
+}
+//5:Given below is an example of a kotlin character array convert to string.
+fun call(str: Array<String>) {
+    val Str = rrayOf('a', 'e', 'i', 'o', 'u')
+}
+
+
+//OOP QUESTION
+//1:Create a class Mobile containing the following state
+//a(Model
+//b(Brand
+//c(MRP
+
+
+//a( print out the detail of the mobile
+//2:Create a class containing firstName,lastName, and age
+//a(Println fullName  and age
+//3:Create a class employee containing the following state
+//a: Name
